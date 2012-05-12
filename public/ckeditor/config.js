bcb92c12b896c62bbfca75327bbd5339f473fbfc
Bartosz Rychlicki <b@br-design.pl>
2012-05-12 16:19:48 +0200

    puszczam CKeditor

---
+++ public/ckeditor/config.js
@@ -0,0 +1,32 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	config.language = 'pl';
+    //config.toolbar = 'Basic';
+	//config.uiColor = '#AADC6E';
+    config.skin = 'office2003';
+
+    config.toolbar = 'ClinicToolbar';
+
+    config.toolbar_ClinicToolbar =
+        [
+            //{ name: 'document', items : [ 'NewPage','Preview' ] },
+            { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+            { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+            //{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
+            //    ,'Iframe' ] },
+            //'/',
+            { name: 'insert', items : [ 'Table' ] },
+
+            //{ name: 'styles', items : [ 'Styles','Format' ] },
+            { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+            { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+            //{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+            { name: 'tools', items : [ 'Maximize','-','About' ] }
+        ];
+};
