b174eaa0d696984feac8a547c7e78141e30d939d
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2012-05-26 11:23:34 +0200

    porzadki w gedmo

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,11 +6,10 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Symfony\\Component\\Yaml' => $vendorDir . '/symfony/yaml/',
-    'Symfony\\Component\\Console' => $vendorDir . '/symfony/console/',
-    'Gedmo' => $vendorDir . '/gedmo/doctrine-extensions/lib/',
-    'Doctrine\\ORM' => $vendorDir . '/doctrine/orm/lib/',
-    'Doctrine\\DBAL' => $vendorDir . '/doctrine/dbal/lib/',
-    'Doctrine\\Common\\DataFixtures' => $vendorDir . '/doctrine/data-fixtures/lib/',
-    'Doctrine\\Common' => $vendorDir . '/doctrine/common/lib/',
+    'Symfony\\Component\\Yaml' => $baseDir . '/vendor/symfony/yaml/',
+    'Symfony\\Component\\Console' => $baseDir . '/vendor/symfony/console/',
+    'Doctrine\\ORM' => $baseDir . '/vendor/doctrine/orm/lib/',
+    'Doctrine\\DBAL' => $baseDir . '/vendor/doctrine/dbal/lib/',
+    'Doctrine\\Common\\DataFixtures' => $baseDir . '/vendor/doctrine/data-fixtures/lib/',
+    'Doctrine\\Common' => $baseDir . '/vendor/doctrine/common/lib/',
 );
