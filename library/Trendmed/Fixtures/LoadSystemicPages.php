abd288f194a3209b4f42266ee04638111ab8e84e
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2012-06-02 14:11:37 +0200

    fixtura dla pages

---
+++ library/Trendmed/Fixtures/LoadSystemicPages.php
@@ -0,0 +1,36 @@
+<?php
+namespace Trendmed\Fixtures;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Trendmed\Entity;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Bard
+ * Date: 13.05.12
+ * Time: 17:30
+ * To change this template use File | Settings | File Templates.
+ */
+class LoadSystemicPages extends \Doctrine\Common\DataFixtures\AbstractFixture implements \Doctrine\Common\DataFixtures\FixtureInterface,
+    \Doctrine\Common\DataFixtures\OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $repository = $manager->getRepository('\Trendmed\Entity\Page');
+        $config = \Zend_Registry::get('config');
+        $page = new \Trendmed\Entity\Page();
+        $page->setTitle('Regulamin dla placówek');
+        $page->setContent('To jest regulamin dla placówek, proszę wydetuj mnie w panelu administratora');
+        $page->setActive(true);
+        $page->setSlug('regulamin-dla-klinik');
+        $page->setIsSystemic(true);
+
+        $manager->persist($page);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 5;
+    }
+}
