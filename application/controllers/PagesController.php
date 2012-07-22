1378b6103ee7f8b182b10487ff6979e0942dec54
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2012-07-22 12:44:18 +0200

    dziala paginacja artykulow w archiwum

--- application/controllers/PagesController.php
+++ application/controllers/PagesController.php
@@ -32,5 +32,49 @@ class PagesController extends Zend_Controller_Action
 
         // TODO: change layout based on type of a page
     }
+
+    /**
+     * Lista artykułów z wyszukiwarką i paginowaną listą
+     */
+    public function archiveAction()
+    {
+        // pobieranie
+        $this->view->headTitle($this->view->translate('Articles list'));
+        $this->_helper->_layout->setLayout('homepage');
+        $config = \Zend_Registry::get('config');
+
+        $request = $this->getRequest();
+        $page = $request->getParam('page', 1);
+        $order = $request->getParam('order', 'created');
+        $direction = $request->getParam('direction', 'DESC');
+
+        // fetching data to paginator
+        $qb = $this->_em->createQueryBuilder()
+            ->select('p')
+            ->from('\Trendmed\Entity\Page', 'p')
+            ->orderBy('p.' . $order, $direction)
+            ->where('p.isActive = ?1')
+            ->setFirstResult(($config->pagination->pages->archiwum * $page) - $config->pagination->pages->archiwum)
+            ->setMaxResults($config->pagination->pages->archiwum);
+
+        $qb->setParameter(1, 1); // only active
+
+
+        $paginator = new Paginator($qb, $fetchJoin = true);
+
+        $c = count($paginator);
+        $i = $config->pagination->pages->archiwum; // items per page
+        $numOfPages = $c / $i;
+        // Making of a Zend_Paginator
+        $zendPaginator = \Zend_Paginator::factory($c);
+        $zendPaginator->setCurrentPageNumber($page);
+        $zendPaginator->setItemCountPerPage($config->pagination->pages->archiwum);
+        $this->view->zendPaginator = $zendPaginator;
+        $this->view->pageCount = $numOfPages;
+        $this->view->articles = $paginator;
+        // adding script path to find pagination file
+        $this->view->addScriptPath(APPLICATION_PATH . '/layouts/scripts/');
+
+    }
 }
 
--- library/Trendmed/Repository/PagesRepository.php
+++ library/Trendmed/Repository/PagesRepository.php
@@ -5,7 +5,8 @@ namespace Trendmed\Repository;
  *
  * @author Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
  */
-class PagesRepository extends \Doctrine\ORM\EntityRepository {
+class PagesRepository extends \Doctrine\ORM\EntityRepository
+{
 
     /**
      * @param int $limit
@@ -19,10 +20,10 @@ class PagesRepository extends \Doctrine\ORM\EntityRepository {
         $qb->select('p')
             ->from('\Trendmed\Entity\Page', 'p');
 
-        switch($limitToType) {
+        switch ($limitToType) {
             case false:
                 $qb->where('p.type = ?1')
-                   ->orWhere('p.type = ?2');
+                    ->orWhere('p.type = ?2');
                 break;
             case 'sponsored':
                 $qb->where('p.type = ?1');
@@ -31,13 +32,16 @@ class PagesRepository extends \Doctrine\ORM\EntityRepository {
                 $qb->where('p.type = ?2');
                 break;
         }
+        $qb->andWhere('p.isActive = ?3'); // for active
         $qb->setParameters(array(
             '1' => 'aritcle_sponsored',
-            '2' => 'article_normal'
+            '2' => 'article_normal',
+            '3' => 1
         ));
         $qb->setMaxResults($limit);
         $query = $qb->getQuery();
         $result = $query->getResult();
         return $result;
     }
+
 }
