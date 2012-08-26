344a061d0a6c3da981f653d58ee2400c12ca5b8a
Bartosz Rychlicki <b@br-design.pl>
2012-08-26 19:01:20 +0200

    akcja dla confirm rezerwacji dla kliniki

---
+++ application/modules/clinic/controllers/ReservationsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This controller takes care of login/logout action for clinic.
+ * It also has some nice actions for password recovery.
+ * Please refer to parent class for more info about the functions.
+ * 
+ * @author Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
+ *
+ */
+class Clinic_ReservationsController extends Me_User_Controllers_LoginController
+{
+    public function confirmAction()
+    {
+
+    }
+}
+
