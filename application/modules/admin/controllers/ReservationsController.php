091ce5ff1a52a6ac18170c0ed41b589d78c83854
Bartosz Rychlicki <b@br-design.pl>
2012-10-17 22:53:45 +0200

    TRENDMED-208 możliwość oznaczenia rezerwacji jako opłaconej z admina

--- application/modules/admin/controllers/ReservationsController.php
+++ application/modules/admin/controllers/ReservationsController.php
@@ -21,12 +21,16 @@ class Admin_ReservationsController extends Zend_Controller_Action {
     {
         $this->view->headTitle('Rezerwacje pacjenta');
         $this->view->patient = $this->_fetchPatientFromParams();
+        $session = new Zend_Session_Namespace('admin_reservations');
+        $session->redirect = $_SERVER['REQUEST_URI'];
     }
 
     public function clinicReservationsAction()
     {
         $this->view->headTitle('Rezerwacje kliniki');
         $this->view->clinic = $this->_fetchClinicFromParams();
+        $session = new Zend_Session_Namespace('admin_reservations');
+        $session->redirect = $_SERVER['REQUEST_URI'];
     }
 
     protected function _fetchPatientFromParams()
@@ -57,6 +61,69 @@ class Admin_ReservationsController extends Zend_Controller_Action {
             throw new \Exception('bad parameters given in '.__FUNCTION__);
         }
         return $user;
+    }
+
+
+    protected function _fetchReservationFromParams()
+    {
+        $request = $this->getRequest();
+        $repo = $this->_em->getRepository('\Trendmed\Entity\Reservation');
+
+        if($request->getParam('id')) {
+            $id             = $request->getParam('id');
+            $entity         = $repo->find($id);
+            if(!$entity) throw new \Exception('No reservation found');
+        } else {
+            throw new \Exception('bad parameters given in '.__FUNCTION__);
+        }
+        return $entity;
+    }
+
+    public function changePayBillStateAction()
+    {
+        $request = $this->getRequest();
+        $newState = $request->getParam('new-state');
+
+        $reservation = $this->_fetchReservationFromParams();
+
+        switch($newState) {
+            case 'paid':
+                $reservation->setBillStatus(\Trendmed\Entity\Reservation::BILL_STATUS_PAID);
+                $this->_helper->FlashMessenger(array(
+                        'info' => 'Rezerwacja oznaczona jako opłacona'
+                    )
+                );
+                break;
+            case 'not-paid':
+                $reservation->setBillStatus(\Trendmed\Entity\Reservation::BILL_STATUS_NOT_PAID);
+                $this->_helper->FlashMessenger(array(
+                        'info' => 'Rezerwacja oznaczona jako nieopłacona'
+                    )
+                );
+                break;
+            case 'not-wanted':
+                $reservation->setBillStatus(\Trendmed\Entity\Reservation::BILL_STATUS_NOT_WANTED);
+                $this->_helper->FlashMessenger(array(
+                        'info' => 'Rezerwacja oznaczona jako niewymagana'
+                    )
+                );
+                break;
+            default:
+                throw new \Exception('Undefined paybill state given in '.__FUNCTION__);
+                break;
+        }
+        $this->_em->persist($reservation);
+        $this->_em->flush();
+
+        // where to redirect after?
+        $session = new Zend_Session_Namespace('admin_reservations');
+        if($session->redirect) {
+            $url = $session->redirect;
+            unset($session->redirect);
+            $this->_redirect($url);
+        } else {
+            $this->_helper->Redirector('index', 'index');
+        }
     }
 }
 ?>
